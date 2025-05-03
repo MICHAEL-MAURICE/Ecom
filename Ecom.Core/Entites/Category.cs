9f70d2b94fb65f7b9b45d95affc2a6b655b036f5
MICHAEL-MAURICE <Michaelmauricengn@gmail.com>
2025-05-03 03:41:38 +0300

    Add project files.

---
+++ Ecom.Core/Entites/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Entites;
+
+public class Category : BaseEntity<int> 
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ICollection<Proudect> Proudects { get; set; } = new HashSet<Proudect>();
+}
---
+++ Ecom.Core/Entites/Proudect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.Entites;
+
+public class Proudect:BaseEntity<int>
+{
+    public string Name { get; set; }=string.Empty;
+    public string Description { get; set; }=string.Empty;
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+}
